74cc3514e7462d62ec4f8e43363d139d53ba076a
AndysArcadium <34086565+AJ-Ora@users.noreply.github.com>
2021-04-14 16:43:21 +0300

    Added rendering to console window for debugging.

--- Game-of-Life.cpp
+++ Game-of-Life.cpp
@@ -2,17 +2,54 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "source/LifeBoard.h"
 #include "source/LifeBoardLogic.h"
+
+void printDebugLoop(LifeBoard& board);
 
 int main()
 {
 	std::cout << "Hello CMake." << std::endl;
 
 	LifeBoard board;
-	LifeBoardLogic logic;
-
-	logic.Step(board);
+	
+	printDebugLoop(board);
 
 	return 0;
+}
+
+void printDebugLoop(LifeBoard& board)
+{
+	LifeBoardLogic logic;
+	bool noCellsAlive = false;
+
+	while (!noCellsAlive)
+	{
+		system("cls");
+
+		logic.Step(board);
+
+		noCellsAlive = true;
+		for (int y = 0; y < board.GetHeight(); ++y)
+		{
+			for (int x = 0; x < board.GetWidth(); ++x)
+			{
+				if (board.GetPixelStatus(x, y) == true)
+				{
+					noCellsAlive = false;
+					std::cout << "o ";
+				}
+				else
+				{
+					std::cout << "  ";
+				}
+			}
+			std::cout << std::endl;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 10));
+	}
 }
