1cfd78706efa980f4aa51565121e48742cc75c6a
AndysArcadium <34086565+AJ-Ora@users.noreply.github.com>
2021-04-16 20:44:01 +0300

    Fixed loaded images always being square shaped.

--- source/LifeBoardDrawing.cpp
+++ source/LifeBoardDrawing.cpp
@@ -30,12 +30,32 @@ void LifeBoardDrawing::OnPaint(wxPaintEvent& evt)
 		return;
 	}
 
+	float ratio = (float)board->GetWidth() / (float)board->GetHeight();
+
+	if (ratio <= 1.0f)
+	{
+		// Width is smaller
+		rescaledWidth = (int)round(rescaledWidth * ratio);
+	}
+	else
+	{
+		// Height is smaller
+		rescaledHeight = (int)round(rescaledHeight / ratio);
+	}
+
 	unsigned char* imageData = board->ToRGB();
 	wxImage image(board->GetWidth(), board->GetHeight(), imageData, true);
 
 	wxBitmap bitmap = wxBitmap(image.Scale(rescaledWidth, rescaledHeight), wxBITMAP_SCREEN_DEPTH);
 
-	dc.DrawBitmap(bitmap, 0, 0, false);
+	if (ratio <= 1.0f)
+	{
+		dc.DrawBitmap(bitmap, (int)round((float)(rescaledHeight - rescaledWidth) / 2.0f), 0, false);
+	}
+	else
+	{
+		dc.DrawBitmap(bitmap, 0, (int)round((float)(rescaledWidth - rescaledHeight) / 2.0f), false);
+	}
 
 	delete[] imageData;
 	evt.Skip();
